2f5d934c1fc06cdbaf5e352e27a97f589c141cd2
ThePrimeagen <samueljamieson02@gmail.com>
2024-10-28 23:28:59 +0000

    the player can shoot a target. if correct they win in not they lose, they can play again when finished

--- main.py
+++ main.py
@@ -9,15 +9,27 @@ def main():
     print(f"Your target has a {target["head"]} head, They have {target["hair"]} hair and they are wearing a {target["shirt"]} shirt")
     looking = gettarget(heads,hair,shirt)
     while shote == False:
-        print(f"You are looking at a person with a {looking["head"]} head, they have {looking["hair"]} hair and they are wearing a {looking["shirt"] shirt.}")
+        print(f"You are looking at a person with a {looking["head"]} head, they have {looking["hair"]} hair and they are wearing a {looking["shirt"]} shirt")
         shoot = input("shoot Y/N? ")
         if shoot.lower() == "n":
             looking = gettarget(heads,hair,shirt)
         elif shoot.lower() == "y":
+            shote = True 
             if target == looking:
                 print("good shot")
             else:
                 print("Wrong person you muppet")
+
+            agian = input("Play Again?: Y/N ")
+            if agian.lower() == "y":
+                main()
+            elif agian.lower() == "n":
+                return 0
+            else:
+                print("invalid ending")
+                return 0
+        else:
+            print("I didnt understand changing targets ")    
      
      
 
