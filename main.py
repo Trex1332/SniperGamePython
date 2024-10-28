4de0e8bc85578823c594c4d611c908103dc57e09
ThePrimeagen <samueljamieson02@gmail.com>
2024-10-28 23:21:09 +0000

    made people you look at. user chooses who to shoot or not. loops through people until correct is found

--- main.py
+++ main.py
@@ -4,10 +4,23 @@ def main():
     heads = ["round","square"]
     hair = ["Blond","Brown"]
     shirt =["red","green","blue"]
-
+    shote = False 
     target = gettarget(heads,hair,shirt)
     print(f"Your target has a {target["head"]} head, They have {target["hair"]} hair and they are wearing a {target["shirt"]} shirt")
- 
+    looking = gettarget(heads,hair,shirt)
+    while shote == False:
+        print(f"You are looking at a person with a {looking["head"]} head, they have {looking["hair"]} hair and they are wearing a {looking["shirt"] shirt.}")
+        shoot = input("shoot Y/N? ")
+        if shoot.lower() == "n":
+            looking = gettarget(heads,hair,shirt)
+        elif shoot.lower() == "y":
+            if target == looking:
+                print("good shot")
+            else:
+                print("Wrong person you muppet")
+     
+     
+
 def gettarget(head,hair,shirt):
     target = {"head":head[random.randrange(0,len(head))], "hair": hair[random.randrange(0,len(hair))],"shirt": shirt[random.randrange(0,len(shirt))]}
     return target 
