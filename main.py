6470b0abf5e22728336606c43580860b13507144
ThePrimeagen <samueljamieson02@gmail.com>
2024-10-28 23:10:31 +0000

    made get target which picks random attributes fron the head, hair and shirts

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import random 
 
 def main():
-    heads = ["round","squar"]
+    heads = ["round","square"]
     hair = ["Blond","Brown"]
     shirt =["red","green","blue"]
 
     target = gettarget(heads,hair,shirt)
-
+    print(f"Your target has a {target["head"]} head, They have {target["hair"]} hair and they are wearing a {target["shirt"]} shirt")
  
- def dettarget(head,hair,shirt):
-    
+def gettarget(head,hair,shirt):
+    target = {"head":head[random.randrange(0,len(head))], "hair": hair[random.randrange(0,len(hair))],"shirt": shirt[random.randrange(0,len(shirt))]}
+    return target 
+main()
