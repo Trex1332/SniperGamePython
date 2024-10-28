9c6add4c2dca7ee5a3afadbace6cd7f4ce674c80
ThePrimeagen <samueljamieson02@gmail.com>
2024-10-28 22:59:39 +0000

    made head hair shirts

---
+++ main.py
@@ -0,0 +1,12 @@
+import random 
+
+def main():
+    heads = ["round","squar"]
+    hair = ["Blond","Brown"]
+    shirt =["red","green","blue"]
+
+    target = gettarget(heads,hair,shirt)
+
+ 
+ def dettarget(head,hair,shirt):
+    
